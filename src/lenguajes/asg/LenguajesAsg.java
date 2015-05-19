ac3e02c60cb77dcf98f041130114344d6da97f70
spideruvm <spideruvm@gmail.com>
2015-05-18 19:45:00 -0500

    Update LenguajesAsg.java

--- src/lenguajes/asg/LenguajesAsg.java
+++ src/lenguajes/asg/LenguajesAsg.java
@@ -16,7 +16,7 @@ public class LenguajesAsg {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Mi primer proyecto");
+        System.out.println("Mi primer proyecto con esta cosa tan Bonita");
     }
     
 }
