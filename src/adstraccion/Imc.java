b6b1cc0b5c633b39118a953f0bd130b56368af32
T-107 <T-107@PC244>
2015-05-28 19:41:10 -0500

    clase 280515

--- src/adstraccion/Imc.java
+++ src/adstraccion/Imc.java
@@ -9,13 +9,36 @@ package adstraccion;
  *
  * @author T-107
  */
+
 public class Imc {
-    float peso;float altura;
-    public Imc(float p, float a){
-        peso=p;altura=a;
-    }
+    private float peso;
+    private float altura;
+    private boolean flaco;
+        
+        public void setFlaco(boolean flaco){
+            this.flaco=flaco;
+        }
+        public boolean getFlaco{
+            return flaco;
+        }
+        public void setPeso(float peso){
+            this.peso=peso;
+        }
+        public void setAltura(float altura){
+            this.altura=altura;
+        }
+        public Imc(float p, float a){
+        peso=p;
+        altura=a;
+        }
+        
+)
+
+ 
     public String calcular(){
         float imc=peso/(altura*altura);
         return "Tu imc es: "+imc;
     }
+
 }
+
--- src/adstraccion/InterfazImc.java
+++ src/adstraccion/InterfazImc.java
@@ -13,5 +13,30 @@ public class InterfazImc {
    public static void main(String oso[]){
        Imc imc=new Imc(80,1.67f);
        System.out.println(imc.calcular()); 
+      // System.out.println(imc.getPeso());
+       
+       //promitivos
+       byte b=2;
+       short s=2;
+       int i=2;//por defecto en java
+       long l=2;
+       
+       b=(byte)s;
+       s=b;
+       i=(int)l;
+       i=s;
+       s=(short)i;
+       
+       float f=2.0f;
+       double d=2.0;
+       
+       boolean valor=false;
+       
+       Imc otro=new Imc();
+       otro.setFlaco(true);
+       System.out.println(otro.isFlaco());
    } 
 }
+
+
+
